329debad40face45de519bdb9b4e7eefb1afb3c6
miralis <an.gredler@hotmail.com>
2017-12-29 13:58:41 +0100

    08/11 - Creating Spring Data Repositories

---
+++ src/main/java/com/example/recipe/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.recipe.repositories;
+
+import com.example.recipe.domain.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+
+}
